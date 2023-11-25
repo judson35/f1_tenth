0d7e6ca3bc8abe15598f0e59d0312aa258f7fac6
Judson Kyle <79773875+judson35@users.noreply.github.com>
2023-11-24 21:27:17 -0500

    Syncing

--- sim_ws/src/waypoint_logger/waypoint_logger/waypoint_logger.py
+++ sim_ws/src/waypoint_logger/waypoint_logger/waypoint_logger.py
@@ -3,10 +3,12 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import PointStamped
+from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import Odometry
 from ackermann_msgs.msg import AckermannDriveStamped
+from tf_transformations import euler_from_quaternion
 
-filename = "Race_2_Map_1_Waypoints.txt"
+filename = "Mpc_Waypoints.txt"
 
 class WaypointLogger(Node):
     def __init__(self):
@@ -17,23 +19,37 @@ class WaypointLogger(Node):
 
         # self.subscription = self.create_subscription(PointStamped, '/clicked_point', self.clicked_point_callback, 1)
         self.subscription = self.create_subscription(Odometry, '/ego_racecar/odom', self.odom_callback, 1)
-        self.timer = self.create_timer(0.1, self.timer_callback_)
+        self.timer = self.create_timer(0.05, self.timer_callback_)
 
         #Start car moving forward
-        msg = AckermannDriveStamped()
-        msg.drive.speed = 1.0
-        self.drive_publisher.publish(msg)
+        # msg = AckermannDriveStamped()
+        # msg.drive.speed = 1.0
+        # self.drive_publisher.publish(msg)
 
         self.x = None
         self.y = None
+        self.yaw = None
+        self.v = None
+        self.qx = None
+        self.qy = None
+        self.qz = None
+        self.qw = None
 
     def timer_callback_(self):
         with open(filename, 'a') as f:
-            f.write(f"{self.x},{self.y}\n")
+            f.write(f"{self.x},{self.y},{self.v},{self.qx},{self.qy},{self.qz},{self.qw}\n")
 
-    def odom_callback(self, msg):
+    def odom_callback(self, msg: Odometry):
         self.x = msg.pose.pose.position.x
         self.y = msg.pose.pose.position.y
+        self.v = msg.twist.twist.linear.x
+
+        self.qx = msg.pose.pose.orientation.x
+        self.qy = msg.pose.pose.orientation.y
+        self.qz = msg.pose.pose.orientation.z
+        self.qw = msg.pose.pose.orientation.w
+
+        _, _, self.yaw = euler_from_quaternion([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
 
 def main(args=None):
     rclpy.init(args=args)
--- sim_ws/src/waypoint_logger/waypoint_logger/waypoint_visualizer.py
+++ sim_ws/src/waypoint_logger/waypoint_logger/waypoint_visualizer.py
@@ -6,8 +6,9 @@ import time
 from geometry_msgs.msg import Point
 from std_msgs.msg import ColorRGBA
 from visualization_msgs.msg import MarkerArray, Marker
+from tf_transformations import quaternion_from_euler
 
-filename = "/home/judson35/f1_tenth/sim_ws/Race_2_Map_1_Waypoints.txt"
+filename = "/home/judson35/f1_tenth/sim_ws/Spielberg_Waypoints.txt"
 
 class WaypointPublisher(Node):
     def __init__(self):
@@ -15,7 +16,7 @@ class WaypointPublisher(Node):
         print("Initializing waypoint publisher")
         print(f"Waypoints Filename: {filename}")
 
-        self.waypoint_publisher = self.create_publisher(Marker, 'target_waypoint', 10)
+        self.waypoint_publisher = self.create_publisher(MarkerArray, 'target_waypoint', 10)
         self.timer = self.create_timer(0.5, self.timer_callback_)
 
         self.waypoints = []
@@ -23,61 +24,64 @@ class WaypointPublisher(Node):
         print("Publisher Initialized")
 
     def read_waypoint_file(self, filename):
-        i = 0
         with open(filename, 'r') as f:
             lines = f.readlines()
-            for line in lines:
-                if (i > 1):
+            for i, line in enumerate(lines):
+                if (i % 5 == 0):
                     data = line.split(',')
-                    self.waypoints.append((float(data[0]), float(data[1])))
-                    i = 0
-                else:
-                    i += 1
+                    self.waypoints.append((float(data[0]), float(data[1]), float(data[2])))
 
     def publish_waypoints(self):
-        marker = Marker()
+        waypoints = MarkerArray()
+        for i, waypoint in enumerate(self.waypoints):
+            marker = Marker()
 
-        marker.type = 7
-        marker.action = 0
-        marker.header.frame_id = "/map"
+            marker.type = 0
+            marker.action = 0
+            marker.header.frame_id = "/map"
 
-        marker.ns = "visualized_waypoints"
+            marker.ns = "mpc_waypoints"
+            marker.id = i
 
-        marker.scale.x = 0.2
-        marker.scale.y = 0.2
-        marker.scale.z = 0.2
+            marker.scale.x = 0.5
+            marker.scale.y = 0.1
+            marker.scale.z = 0.1
 
-        marker.color.r = 0.0
-        marker.color.g = 1.0
-        marker.color.b = 0.0
-        marker.color.a = 1.0
+            marker.color.r = 0.0
+            marker.color.g = 1.0
+            marker.color.b = 0.0
+            marker.color.a = 1.0
 
-        marker.pose.position.x = 0.0
-        marker.pose.position.y = 0.0
-        marker.pose.position.z = 0.0
+            marker.pose.position.x = waypoint[0]
+            marker.pose.position.y = waypoint[1]
+            marker.pose.position.z = 0.0
 
-        marker.pose.orientation.x = 0.0
-        marker.pose.orientation.y = 0.0
-        marker.pose.orientation.z = 0.0
-        marker.pose.orientation.w = 1.0
+            angle = quaternion_from_euler(0.0, 0.0, waypoint[2])
 
-        for waypoint in self.waypoints:
-            point = Point()
-            color = ColorRGBA()
+            marker.pose.orientation.x = angle[0]
+            marker.pose.orientation.y = angle[1]
+            marker.pose.orientation.z = angle[2]
+            marker.pose.orientation.w = angle[3]
 
-            point.x = waypoint[0]
-            point.y = waypoint[1]
-            point.z = 0.0
+            waypoints.markers.append(marker)
 
-            color.r = 0.0
-            color.g = 1.0
-            color.b = 0.0
-            color.a = 1.0
+            # for waypoint in self.waypoints:
+            #     point = Point()
+            #     color = ColorRGBA()
 
-            marker.points.append(point)
-            marker.colors.append(color)
+            #     point.x = waypoint[0]
+            #     point.y = waypoint[1]
+            #     point.z = 0.0
+
+            #     color.r = 0.0
+            #     color.g = 1.0
+            #     color.b = 0.0
+            #     color.a = 1.0
+
+            #     marker.points.append(point)
+            #     marker.colors.append(color)
         marker.lifetime.sec = 1
-        self.waypoint_publisher.publish(marker)
+        self.waypoint_publisher.publish(waypoints)
 
     def timer_callback_(self):
         self.publish_waypoints()
